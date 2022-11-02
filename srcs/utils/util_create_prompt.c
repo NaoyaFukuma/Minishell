fc46b985db4506e8c4d7b80fa418637df6e28dd0
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2022-11-02 13:01:26 +0900

    add create_prompt.c

--- srcs/utils/util_create_prompt.c
+++ srcs/utils/util_create_prompt.c
@@ -6,14 +6,11 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 14:43:44 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/10/31 12:24:04 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/10/31 17:11:02 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static char	*join_and_free_err_exit(char *str1, bool free_str1, char *str2,
-				bool free_str2);
 
 char	*util_create_prompt_str(void)
 {
@@ -22,22 +19,24 @@ char	*util_create_prompt_str(void)
 	char			*tmp_str;
 
 	if (g_shell.status == 0)
-		ret_str = join_and_free_err_exit(DEFAULT_COLOR, false, "[", false);
+		ret_str = util_join_and_free_err_exit("\n", false, DEFAULT_COLOR, false);
 	else if (0 < g_shell.status && g_shell.status < 128)
-		ret_str = join_and_free_err_exit(RED_COLOR, false, "[", false);
+		ret_str = util_join_and_free_err_exit("\n", false, RED_COLOR, false);
 	else
-		ret_str = join_and_free_err_exit(YELLOW_COLOR, false, "[", false);
+		ret_str = util_join_and_free_err_exit("\n", false, YELLOW_COLOR, false);
 	tmp_str = ft_itoa(g_shell.status);
 	if (!tmp_str)
 		util_put_cmd_err_and_exit("in create_prompt_str");
-	ret_str = join_and_free_err_exit(ret_str, true, tmp_str, true);
-	ret_str = join_and_free_err_exit(ret_str, true, "] \e[m", false);
-	ret_str = join_and_free_err_exit(ret_str, true, g_shell.pwd, false);
-	ret_str = join_and_free_err_exit(ret_str, true, " > ", false);
+	ret_str = util_join_and_free_err_exit(ret_str, true, "[", false);
+	ret_str = util_join_and_free_err_exit(ret_str, true, tmp_str, true);
+	ret_str = util_join_and_free_err_exit(ret_str, true, "] \e[m", false);
+	ret_str = util_join_and_free_err_exit(ret_str, true, g_shell.pwd, false);
+	ret_str = util_join_and_free_err_exit(ret_str, true, " > ", false);
+	g_shell.prompt_len = ft_strlen(ret_str) - 9;
 	return (ret_str);
 }
 
-static char	*join_and_free_err_exit(char *str1, bool free_str1, char *str2,
+char	*util_join_and_free_err_exit(char *str1, bool free_str1, char *str2,
 		bool free_str2)
 {
 	char	*ret_str;
