9592a1630169ba784bd885df548bddd3f5a9f7d8
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2022-11-02 12:59:52 +0900

    add signal.c

--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 23:42:47 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/10/31 10:38:34 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/11/02 10:12:12 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,24 @@ static void	sig_int_handle(int sig);
 void	set_sig_for_interactive_shell(void)
 {
 	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR || signal(SIGINT, sig_int_handle) == SIG_ERR)
-		util_put_cmd_err_and_exit("set signal");
+		util_put_cmd_err_and_exit("in set_sig_for_interactive_shell()");
+}
+
+void	set_sig_for_cmd_running(void)
+{
+	if (signal(SIGQUIT, SIG_DFL) == SIG_ERR || signal(SIGINT, SIG_DFL) == SIG_ERR)
+		util_put_cmd_err_and_exit("in set_sig_for_cmd_running()");
 }
 
 static void	sig_int_handle(int sig)
 {
-	write(STDOUT_FILENO, "\n", 1);
-	rl_replace_line("", 0);
+	extern t_shell	g_shell;
+
+	(void)sig;
 	rl_on_new_line();
+	rl_replace_line("", 0);
+	write(STDOUT_FILENO, "\n", 1);
 	rl_redisplay();
-	util_set_status(128 + sig);
+	util_set_status(1);
+	g_shell.interrupted = true;
 }
