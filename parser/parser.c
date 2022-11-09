5b653087df145ceb2fa458fefe0d1f139564d47a
chanma <yunosuke.homma@icloud.com>
2022-11-10 00:04:30 +0900

    input_cmd_args fin?

--- parser/parser.c
+++ parser/parser.c
@@ -31,25 +31,37 @@ t_token_list	*duplicate_token(t_token_list *token)
 	return (dup_token);
 }
 
-void	func(t_token_list **cmd_args, t_token_list **token)
+void	add_token_into_cmd_args(t_token_list **cmd_args, t_token_list **token)
 {
-	//cmd_argsにtokenの一つ目をまるまるコピーしちゃう
+	//cmd_argsにtokenの一つ目をまるまるコピーしたい
 	//->するとtokenの一つ目のコピーを作ってあげて代入する必要がある
 	t_token_list	*dup_token;
+	//元のcmd_argsのアドレスを動かしたくないから別で最後尾のアドレスを持つ変数を宣言
+	t_token_list	*cmd_args_tail;
 
 	dup_token = duplicate_token(*token);
 	if (!*cmd_args)
 		*cmd_args = dup_token;
 	//先頭じゃなかった場合は
 	else
-
+	{
+		cmd_args_tail = *cmd_args;
+		while (cmd_args_tail->next)
+		{
+			cmd_args_tail = cmd_args_tail->next;
+		}
+		cmd_args_tail->next = dup_token;
+		/* ここがわからない
+		dup_token->prev = cmd_args_tail->next;
+		*/
+	}
 }
 
 void	input_cmd_args(t_command *command, t_token_list **token)
 {
 	while (*token && (*token)->type == TOKEN)
 	{
-		func(&command->args, token);
+		add_token_into_cmd_args(&command->args, token);
 		*token = (*token)->next;
 	}
 }
